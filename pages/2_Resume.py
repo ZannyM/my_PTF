f036a18f95af8f2b92926a341433255bbce52766
zmaholobela023 <zmaholobela023@student.wethinkcode.co.za>
2024-04-02 14:40:28 +0200

    first commit

--- pages/2_Resume.py
+++ pages/2_Resume.py
@@ -12,7 +12,7 @@ st.sidebar.markdown(info['Photo'],unsafe_allow_html=True)
 
 st.title("Resume")
 
-with open("images/resume.pdf","rb") as f:
+with open("resume/resume.pdf","rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
       pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
       st.markdown(pdf_display, unsafe_allow_html=True)
--- pages/3_Hobbies.py
+++ pages/3_Hobbies.py
@@ -2,7 +2,8 @@ import streamlit as st
 from PIL import Image
 from information import *
 import json
-
+from streamlit_lottie import st_lottie
+#https://github.com/ZannyM/Myportfolio.git
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
@@ -11,20 +12,44 @@ local_css("style/style.css")
 
 st.sidebar.markdown(info['Photo'],unsafe_allow_html=True)
 
-# img_1 = Image.open("images/1.jpg")
-# img_2 = Image.open("images/2.png")
-# img_3 = Image.open("images/3.png")
 
 st.title("Hobbies")
 
-# col1, col2, col3 = st.columns(3)
+file_path_gym = '/home/zannymaholobela/Downloads/Animation - 1712058518157.json'
+file_path_reading = '/home/zannymaholobela/Downloads/Animation - 1712058793497.json'
+file_path_painting = '/home/zannymaholobela/Downloads/Animation - 1712059173535.json'
+file_path_travel = '/home/zannymaholobela/Downloads/Animation - 1712059539579.json'
 
-# with col1:
-#    st.image(img_1)
-   
-# with col2:
-#    st.image(img_2)
+#read contents of local file
+with open(file_path_gym, 'r') as file:
+    animation_data1 = json.load(file)
 
-# with col3:
-#    st.image(img_3)
+with open(file_path_reading, 'r') as file:
+    animation_data2 = json.load(file)
+
+with open(file_path_painting, 'r') as file:
+    animation_data3 = json.load(file)
+
+with open(file_path_travel, 'r') as file:
+    animation_data4 = json.load(file)
+
+with st.container():
+    
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+    with col1:
+        st_lottie(animation_data1, height=280, key="gym")
+
+    with col2:
+        st_lottie(animation_data2, height=280, key="reading")
+
+    with col3:
+        st_lottie(animation_data3, height=280, key="painting")
+
+    with col4:
+        st_lottie(animation_data4, height=280, key="travel")
+
+
+ 
+
+
 
